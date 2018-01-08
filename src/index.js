cfd8273116d2024df2587909ea061d74c781354c
Brandon Thomas <brandon@growlabs.com>
2018-01-08 13:12:39 -0800

    add submit and submit handler

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './styles/style.sass';
+import $ from 'jquery';
+
+const handleSubmit = () => {
+  const $form = $('#reserve-form');
+  const data = $form.serialize();
+
+  console.log(data);
+
+  $.post('http://api.yukine.me/blfc/riders', data)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch(console.error);
+};
+
+const init = () => {
+  const $submit = $('#reserve-submit');
+
+  $submit.on('click', handleSubmit);
+};
+
+$(init);
