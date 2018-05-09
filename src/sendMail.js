c22d94e30920a13c2b028bbebd61d12722961c55
Brandon <lucky7id@gmail.com>
2018-05-08 19:41:59 -0700

    let 'er rip

--- src/sendMail.js
+++ src/sendMail.js
@@ -10,8 +10,6 @@ const mailer = new Mailer();
 db.getConfirmedCount()
   .then((riders) => {
       riders.forEach(rider => {
-        if (rider.email !== 'lucky7id@gmail.com') return;
-
         mailer.sendFinal(rider.email, rider.char_name);
         console.log(`Emailed: ${rider.email} ${rider.char_name}`);
       });
